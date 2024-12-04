594e7057a04de3a617c369f94dc2978ba5cfeb20
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-04 18:40:06 +0900

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -106,7 +106,7 @@ resource "azurerm_lb_rule" "lb_http_rule" {
   frontend_port                  = 80
   backend_port                   = 80
   frontend_ip_configuration_name = "frontend"
-  backend_address_pool_ids        = azurerm_lb_backend_address_pool.lb_backend_pool.id
+  backend_address_pool_ids        = [azurerm_lb_backend_address_pool.lb_backend_pool.id]
   probe_id                       = azurerm_lb_probe.lb_http_probe.id
 }
 
