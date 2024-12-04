93931ca2a0ea68977aa430f17b9bcd2f7c2de6ae
Ubuntu <ubuntu@ip-192-168-55-188.ap-northeast-1.compute.internal>
2024-12-04 09:05:59 +0000

    add code for vmss

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -20,3 +20,86 @@ resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_config.name
   location = var.resource_group_config.location
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "udacity-vmss-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "udacity-vmss-vnet-subnet"
+  resource_group_name  = azurerm_resource_group.resource_group.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "udacity-vmss-nsg"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
+  name                = "vmss-example"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  admin_username      = "azureuser"
+  admin_password      = "Password1234!"
+  instances           = 2
+  sku                 = "Standard_DS1_v2"
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  network_interface {
+    name    = "vmss-nic"
+    primary = true
+
+    ip_configuration {
+      name                                   = "vmss-ip-config"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.subnet.id
+      load_balancer_backend_address_pool_ids = []
+    }
+  }
+
+  tags = {
+    environment = "Test"
+  }
+}
+
