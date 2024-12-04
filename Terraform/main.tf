0d17f2f54319b3c9b05a854e4c97a848a142188b
Ubuntu <ubuntu@ip-192-168-55-188.ap-northeast-1.compute.internal>
2024-12-04 09:30:27 +0000

    fix code

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -65,6 +65,57 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
+resource "azurerm_public_ip" "lb_public_ip" {
+  name                = "vmss-lb-public-ip"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_lb" "lb" {
+  name                = "vmss-example-lb"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  sku                 = "Standard"
+  frontend_ip_configuration {
+    name                 = "frontend"
+    public_ip_address_id = azurerm_public_ip.lb_public_ip.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "lb_backend_pool" {
+  name                = "backend-pool"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  loadbalancer_id     = azurerm_lb.lb.id
+}
+
+resource "azurerm_lb_probe" "lb_http_probe" {
+  name                = "http-probe"
+  resource_group_name = azurerm_resource_group.resource_group..name
+  loadbalancer_id     = azurerm_lb.lb.id
+  protocol            = "Http"
+  port                = 80
+  request_path        = "/"
+  interval_in_seconds = 5
+  number_of_probes    = 2
+}
+
+resource "azurerm_lb_rule" "lb_http_rule" {
+  name                           = "http-rule"
+  resource_group_name            = azurerm_resource_group.resource_group..name
+  loadbalancer_id                = azurerm_lb.lb.id
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "frontend"
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.lb_backend_pool.id
+  probe_id                       = azurerm_lb_probe.lb_http_probe.id
+}
+
+
+
+
 resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
   name                = "vmss-example"
   location            = azurerm_resource_group.resource_group.location
@@ -99,7 +150,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
       name                                   = "vmss-ip-config"
       primary                                = true
       subnet_id                              = azurerm_subnet.subnet.id
-      load_balancer_backend_address_pool_ids = []
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.lb_backend_pool.id]
     }
   }
 
