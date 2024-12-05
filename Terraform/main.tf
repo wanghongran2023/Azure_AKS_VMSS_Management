c7f124477f5e4db0ff6020e7b2078c2804acdb7d
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-05 19:06:05 +0900

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -128,3 +128,17 @@ resource "azurerm_virtual_machine_scale_set_extension" "vmss_ext_mma" {
     "stopOnMultipleConnections" = true
   })
 }
+
+resource "azurerm_virtual_machine_scale_set_extension" "vmss_ext_da" {
+  virtual_machine_scale_set_id = azurerm_linux_virtual_machine_scale_set.vmss.id
+  auto_upgrade_minor_version   = true
+  name                         = "DependencyAgentLinux"
+  publisher                    = "Microsoft.Azure.Monitoring.DependencyAgent"
+  type                         = "DependencyAgentWindows"
+  type_handler_version         = "9.10"
+  provision_after_extensions = [azurerm_virtual_machine_scale_set_extension.vmss_ext_mma.name]
+
+  settings = jsonencode({
+    "enableAutomaticUpgrade" = true
+  })
+}
