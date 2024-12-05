26998dda2143a73246071f99bb4d2ae2b186d2a7
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-05 11:58:53 +0900

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -107,7 +107,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
     ip_configuration {
       name                                   = "vmss-ip-config"
       primary                                = true
-      subnet_id                              = azurerm_subnet.subnet.id
+      subnet_id                              = module.network.subnetid
       load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.lb_backend_pool.id]
     }
   }
