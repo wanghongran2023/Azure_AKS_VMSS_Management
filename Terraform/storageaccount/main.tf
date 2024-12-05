37ac2798f4005361fc66b8a851054fefb29c68b7
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-05 12:49:20 +0900

    Update main.tf

--- Terraform/storageaccount/main.tf
+++ Terraform/storageaccount/main.tf
@@ -1,1 +1,11 @@
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "storageaccount"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS" # Locally-redundant storage (LRS)
 
+  tags = {
+    environment = "Test"
+  }
+}
