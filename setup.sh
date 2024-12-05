22d97f42b37fced5aa56fcd6517747aa93a4aa83
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-05 23:07:02 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ wget https://download.redis.io/releases/redis-6.2.4.tar.gz
 tar xzf redis-6.2.4.tar.gz
 cd redis-6.2.4
 make
-sudo apt install redis-tools
-sudo apt install redis-server
+sudo apt install -y redis-tools
+sudo apt install -y redis-server
 redis-server --daemonize yes
 cd ..      
 pip install -r requirements.txt
