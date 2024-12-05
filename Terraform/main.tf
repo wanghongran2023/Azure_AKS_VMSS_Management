7340e230735835c6838715ebf472e50a08f84b4d
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-05 12:20:57 +0900

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -27,50 +27,6 @@ module "network" {
   resource_group_location=azurerm_resource_group.resource_group.location
 }
 
-resource "azurerm_public_ip" "lb_public_ip" {
-  name                = "vmss-lb-public-ip"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_lb" "lb" {
-  name                = "udacity-vmss-lb"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  sku                 = "Standard"
-  frontend_ip_configuration {
-    name                 = "frontend"
-    public_ip_address_id = azurerm_public_ip.lb_public_ip.id
-  }
-}
-
-resource "azurerm_lb_backend_address_pool" "lb_backend_pool" {
-  name                = "udacity-vmss-backend-pool"
-  loadbalancer_id     = azurerm_lb.lb.id
-}
-
-resource "azurerm_lb_probe" "lb_http_probe" {
-  name                = "udacity-vmss-http-probe"
-  loadbalancer_id     = azurerm_lb.lb.id
-  protocol            = "Http"
-  port                = 80
-  request_path        = "/"
-  interval_in_seconds = 5
-  number_of_probes    = 2
-}
-
-resource "azurerm_lb_rule" "lb_http_rule" {
-  name                           = "udacity-vmss-http-rule"
-  loadbalancer_id                = azurerm_lb.lb.id
-  protocol                       = "Tcp"
-  frontend_port                  = 80
-  backend_port                   = 80
-  frontend_ip_configuration_name = "frontend"
-  backend_address_pool_ids        = [azurerm_lb_backend_address_pool.lb_backend_pool.id]
-  probe_id                       = azurerm_lb_probe.lb_http_probe.id
-}
 
 
 
