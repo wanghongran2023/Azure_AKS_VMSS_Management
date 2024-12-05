00c75f906220341f6575b772aa7ced7ef2ca7aa3
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-05 12:24:49 +0900

    Update outputs.tf

--- Terraform/loadbalancer/outputs.tf
+++ Terraform/loadbalancer/outputs.tf
@@ -1,4 +1,4 @@
 output "backend_pool_id" {
   description = "The ID of the loadbalancer backend pool"
-  value       = azurerm_subnet.subnet.id
+  value       = azurerm_lb_backend_address_pool.lb_backend_pool.id
 }
