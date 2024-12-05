ad96ca3a6dab01afeb902f9b488b534db917eba6
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-05 12:39:03 +0900

    Update main.tf

--- Terraform/network/main.tf
+++ Terraform/network/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = "udacity-vmss-vnet"
+  name                = "${var.resource_header}-vnet"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
   address_space       = ["10.0.0.0/16"]
@@ -10,14 +10,14 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "udacity-vmss-vnet-subnet"
+  name                 = "${var.resource_header}-vnet-subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_security_group" "nsg" {
-  name                = "udacity-vmss-nsg"
+  name                = "${var.resource_header}-nsg"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
 
