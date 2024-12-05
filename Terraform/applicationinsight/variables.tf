f806a6b16793b4daa7f09bf3fc4e15ae04996fac
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-05 18:54:42 +0900

    Update variables.tf

--- Terraform/applicationinsight/variables.tf
+++ Terraform/applicationinsight/variables.tf
@@ -12,3 +12,13 @@ variable "resource_header" {
   description = "The Header of the resource name"
   type        = string
 }
+
+variable "log_analytics_workspace_id" {
+  description = "The id of the analytics workspace"
+  type        = string
+}
+
+variable "log_analytics_workspace_name" {
+  description = "The name of the analytics workspace"
+  type        = string
+}
