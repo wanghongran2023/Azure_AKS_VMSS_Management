1e081c581bea83bcd893ae96cc873ca7ccd23033
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-05 19:03:38 +0900

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -111,3 +111,20 @@ resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
   }
 }
 
+resource "azurerm_virtual_machine_scale_set_extension" "vmss_ext_mma" {
+  virtual_machine_scale_set_id = azurerm_linux_virtual_machine_scale_set.vmss.id
+  auto_upgrade_minor_version   = true
+  name                         = "MicrosoftMonitoringAgent"
+  publisher                    = "Microsoft.EnterpriseCloud.Monitoring"
+  type                         = "MicrosoftMonitoringAgent"
+  type_handler_version         = "1.0"
+  
+  protected_settings = jsonencode({
+    "workspaceKey" = "${azurerm_log_analytics_workspace.law.primary_shared_key}"
+  })
+
+  settings = jsonencode({
+    "workspaceId"               = "${data.azurerm_log_analytics_workspace.example.id}",
+    "stopOnMultipleConnections" = true
+  })
+}
