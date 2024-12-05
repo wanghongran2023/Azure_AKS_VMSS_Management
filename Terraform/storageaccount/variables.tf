c98e4204f000fda033874562e2ab466a6f2fd5dc
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-05 12:50:07 +0900

    Create variables.tf

---
+++ Terraform/storageaccount/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "resource_group_location" {
+  description = "The location of the resource group"
+  type        = string
+}
+
+variable "resource_header" {
+  description = "The Header of the resource name"
+  type        = string
+}
