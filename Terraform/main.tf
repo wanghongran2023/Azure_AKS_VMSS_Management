da3df2810d21ec09c885f95b5dc8c63008a15f81
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-05 18:47:03 +0900

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -135,28 +135,32 @@ resource "azurerm_monitor_data_collection_rule" "example" {
   location            = azurerm_resource_group.resource_group.location
 
   data_flow {
-    streams {
-      name = "PerformanceCounters"
-
-      performance_counters {
-        counter  = "% Processor Time"
-        instance = "_Total"
-        name     = "Processor"
+    streams = [
+      {
+        name = "PerformanceCounters"
+        performance_counters = [
+          {
+            counter  = "% Processor Time"
+            instance = "_Total"
+            name     = "Processor"
+          },
+          {
+            counter  = "Available MBytes"
+            instance = "_Total"
+            name     = "Memory"
+          }
+        ]
       }
+    ]
 
-      performance_counters {
-        counter  = "Available MBytes"
-        instance = "_Total"
-        name     = "Memory"
+    destinations = [
+      {
+        log_analytics = {
+          name                    = "example-workspace"
+          workspace_resource_id   = data.azurerm_log_analytics_workspace.example.id
+        }
       }
-    }
-  }
-
-  destinations {
-    log_analytics {
-      name                    = "example-workspace"
-      workspace_resource_id = data.azurerm_log_analytics_workspace.example.id
-    }
+    ]
   }
 }
 
