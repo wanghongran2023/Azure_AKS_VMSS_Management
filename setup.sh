ac580ae29230bf1229bcacbbce10220f4a8976e2
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-05 22:46:09 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt update     
+sudo apt install python-pip -y
+wget https://download.redis.io/releases/redis-6.2.4.tar.gz
+tar xzf redis-6.2.4.tar.gz
+cd redis-6.2.4
+make
+
+cd ..      
+pip install -r requirements.txt
+cd azure-vote/      
+python main.py
