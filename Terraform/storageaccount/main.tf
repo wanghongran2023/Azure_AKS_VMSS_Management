6a948e80ca82f1e6a87df4ebdb011725f0ca07d9
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-05 12:56:17 +0900

    Update main.tf

--- Terraform/storageaccount/main.tf
+++ Terraform/storageaccount/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage_account" {
-  name                = "${var.resource_header}-storageaccount"
+  name                = "${var.resource_header}storage"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
   account_tier             = "Standard"
