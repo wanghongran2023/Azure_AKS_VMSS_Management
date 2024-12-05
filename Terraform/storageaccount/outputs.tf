24c860cbeaab696e0c38b3fdae967a5303d41be3
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-05 12:50:55 +0900

    Update outputs.tf

--- Terraform/storageaccount/outputs.tf
+++ Terraform/storageaccount/outputs.tf
@@ -1,7 +1,7 @@
 output "storage_account_name" {
-  value = azurerm_storage_account.example.name
+  value = azurerm_storage_account.storage_account.name
 }
 
 output "storage_account_primary_key" {
-  value = azurerm_storage_account.example.primary_access_key
+  value = azurerm_storage_account.storage_account.primary_access_key
 }
