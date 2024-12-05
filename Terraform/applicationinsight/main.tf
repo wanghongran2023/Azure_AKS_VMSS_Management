addebaef692b38aebf8b2f113dddf218febb95c1
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-05 12:36:58 +0900

    Update main.tf

--- Terraform/applicationinsight/main.tf
+++ Terraform/applicationinsight/main.tf
@@ -1,1 +1,11 @@
+resource "azurerm_application_insights" "example" {
+  name                = "${var.resource_header}-appinsights"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  application_type    = "web"
 
+  # Optional tags
+  tags = {
+    environment = "development"
+  }
+}
