ec845d7755fb22f348d397a08faf5ec941877f9f
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-05 21:21:31 +0900

    Update outputs.tf

--- Terraform/loadbalancer/outputs.tf
+++ Terraform/loadbalancer/outputs.tf
@@ -2,3 +2,8 @@ output "backend_pool_id" {
   description = "The ID of the loadbalancer backend pool"
   value       = azurerm_lb_backend_address_pool.lb_backend_pool.id
 }
+
+output "lb_http_probe" {
+  description = "The ID of the lb http probe"
+  value       = azurerm_lb_probe.lb_http_probe.id
+}
