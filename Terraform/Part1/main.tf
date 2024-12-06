0658aea6039d776ff63501ed27b48377e959184d
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-06 18:08:57 +0900

    Update main.tf

--- Terraform/Part1/main.tf
+++ Terraform/Part1/main.tf
@@ -16,8 +16,9 @@ provider "azurerm" {
   features {}
 }
 
-data "azurerm_resource_group" "resource_group" {
+resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_config.name
+  location     = var.resource_group_config.location
 }
 
 resource "azurerm_application_insights" "appinsights" {
