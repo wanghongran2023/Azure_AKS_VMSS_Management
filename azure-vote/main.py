0ec785465db5e2329c627ecbf1fe56e77f8ae241
Github Action <githubaction@example.com>
2024-12-06 01:22:22 +0000

    Update main.py with new key

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -24,19 +24,19 @@ from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 
 # Logging
 logger = logging.getLogger(__name__)
-handler = AzureLogHandler(connection_string='InstrumentationKey=cbc1cc0e-28b6-4990-bc7f-0afe1e1b7f33')
+handler = AzureLogHandler(connection_string='InstrumentationKey=9f753fd9-d22c-4043-9f03-d174286e9ea8')
 handler.setFormatter(logging.Formatter('%(traceId)s %(spanId)s %(message)s'))
 logger.addHandler(handler)
 
 # Metrics
 exporter = metrics_exporter.new_metrics_exporter(
     enable_standard_metrics=True,
-    connection_string='InstrumentationKey=cbc1cc0e-28b6-4990-bc7f-0afe1e1b7f33'
+    connection_string='InstrumentationKey=9f753fd9-d22c-4043-9f03-d174286e9ea8'
 )
 
 # Tracing
 tracer = Tracer(
-    exporter=AzureExporter(connection_string='InstrumentationKey=cbc1cc0e-28b6-4990-bc7f-0afe1e1b7f33'),
+    exporter=AzureExporter(connection_string='InstrumentationKey=9f753fd9-d22c-4043-9f03-d174286e9ea8'),
     sampler=ProbabilitySampler(1.0)
 )
 
@@ -45,7 +45,7 @@ app = Flask(__name__)
 # Requests
 middleware = FlaskMiddleware(
     app,
-    exporter=AzureExporter(connection_string='InstrumentationKey=cbc1cc0e-28b6-4990-bc7f-0afe1e1b7f33'),
+    exporter=AzureExporter(connection_string='InstrumentationKey=9f753fd9-d22c-4043-9f03-d174286e9ea8'),
     sampler=ProbabilitySampler(rate=1.0)
 )
 
