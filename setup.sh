6211390e5a555a6a251f41cf4fd2142993c2c193
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-06 21:54:31 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 set -e  
-sudo apt update -y
-sudo apt install -y python-pip
+
 wget -q https://download.redis.io/releases/redis-6.2.4.tar.gz
 tar xzf redis-6.2.4.tar.gz
 cd redis-6.2.4
@@ -10,6 +9,16 @@ make
 cd ..
 sudo apt install -y redis-tools redis-server
 sudo redis-server --daemonize yes
-pip install -r requirements.txt
+
+sudo apt update -y
+sudo apt install python3-pip -y
+sudo apt-get install python3-venv -y
+python3 -m venv venv
+source venv/bin/activate
+
+sudo python3 -m pip install --upgrade pip setuptools wheel
+sudo python3 -m pip install setuptools-rust
+sudo pip3 install -r requirements.txt
+
 cd azure-vote/
 sudo nohup python main.py > app.log 2>&1 &
