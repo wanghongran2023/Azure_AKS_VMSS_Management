64b5cf9457319fd9eb1d89c0d04db0ce3ea9c9e3
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-06 12:31:49 +0900

    Update outputs.tf

--- Terraform/Part1/outputs.tf
+++ Terraform/Part1/outputs.tf
@@ -1,4 +1,4 @@
-output "instrumentation_key" {
-  value = azurerm_application_insights.appinsights.instrumentation_key
+output "connection_string" {
+  value     = azurerm_application_insights.appinsights.connection_string
   sensitive = true
 }
