2187e3551192fc1052ef64cb8c2425882b01a200
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-06 23:02:49 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,13 @@
 
 set -e  
 
+sudo apt update -y
+sudo apt install python3-pip -y
+sudo -H pip3 install --upgrade pip
+sudo apt-get install python3-venv -y
+python3 -m venv venv
+source venv/bin/activate
+
 wget -q https://download.redis.io/releases/redis-6.2.4.tar.gz
 tar xzf redis-6.2.4.tar.gz
 cd redis-6.2.4
@@ -9,12 +16,6 @@ make
 cd ..
 sudo apt install -y redis-tools redis-server
 sudo redis-server --daemonize yes
-
-sudo apt update -y
-sudo apt install python3-pip -y
-sudo apt-get install python3-venv -y
-python3 -m venv venv
-source venv/bin/activate
 
 sudo python3 -m pip install --upgrade pip setuptools wheel
 sudo python3 -m pip install setuptools-rust
