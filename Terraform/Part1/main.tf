2032990fa2a752a1f402e08a682018387a74bef0
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-07 16:21:22 +0900

    Update main.tf

--- Terraform/Part1/main.tf
+++ Terraform/Part1/main.tf
@@ -23,7 +23,7 @@ data "azurerm_resource_group" "resource_group" {
 
 resource "azurerm_application_insights" "appinsights" {
   name                = "app-insights"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = data.azurerm_resource_group.resource_group.location
+  resource_group_name = data.azurerm_resource_group.resource_group.name
   application_type    = "web"
 }
