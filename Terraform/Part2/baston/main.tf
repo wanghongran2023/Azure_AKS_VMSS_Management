80560bd6afdb6c5e1226564429bf30641b38a124
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-07 20:48:20 +0900

    Update main.tf

--- Terraform/Part2/baston/main.tf
+++ Terraform/Part2/baston/main.tf
@@ -1,13 +1,13 @@
-resource "azurerm_public_ip" "bastonip" {
-  name                = "baston-public-ip"
+resource "azurerm_public_ip" "bastionip" {
+  name                = "bastion-public-ip"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
 }
 
 
-resource "azurerm_network_interface" "bastonnic" {
-  name                = "baston-nic"
+resource "azurerm_network_interface" "bastionnic" {
+  name                = "bastion-nic"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
 
@@ -15,15 +15,15 @@ resource "azurerm_network_interface" "bastonnic" {
     name                          = "example-ip-config"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.bastonip.id
+    public_ip_address_id          = azurerm_public_ip.bastionip.id
   }
 }
 
 resource "azurerm_linux_virtual_machine" "baston" {
-  name                  = "baston-vm"
+  name                  = "bastion-vm"
   location              = var.resource_group_location
   resource_group_name   = var.resource_group_name
-  network_interface_ids = [azurerm_network_interface.bastonnic.id]
+  network_interface_ids = [azurerm_network_interface.bastionnic.id]
   size                  = "Standard_DS1_v2"
 
   admin_username = "exampleadmin"
