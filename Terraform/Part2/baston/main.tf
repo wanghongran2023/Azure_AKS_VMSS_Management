d002fd0ae3a72f4bf89fa46d44e05715ad0b44ef
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-07 20:48:32 +0900

    Update main.tf

--- Terraform/Part2/baston/main.tf
+++ Terraform/Part2/baston/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_linux_virtual_machine" "baston" {
   network_interface_ids = [azurerm_network_interface.bastionnic.id]
   size                  = "Standard_DS1_v2"
 
-  admin_username = "exampleadmin"
+  admin_username = "admin"
   admin_password = "ExampleP@ssword123!!"
 
   disable_password_authentication = false
