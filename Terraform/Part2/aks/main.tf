feea06cac7b041cd7167c91fa9352681d15432d6
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-07 09:33:02 +0900

    Update main.tf

--- Terraform/Part2/aks/main.tf
+++ Terraform/Part2/aks/main.tf
@@ -1,22 +1,15 @@
-
-
 resource "azurerm_container_registry" "acr" {
-  name                = var.acr_config.name
-  resource_group_name = azurerm_resource_group.resource_group.name
-  location            = azurerm_resource_group.resource_group.location
+  name                = "${var.resource_header}-acr"
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
   sku                 = "Basic"
   admin_enabled       = true
 }
 
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-  sensitive = true
-}
-
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_config.name
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
+  name                = "${var.resource_header}-aks"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
   dns_prefix          = "exampleaks"
 
   default_node_pool {
