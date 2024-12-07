8073f4c92a33c25e578fbb6d82e5a3cf4afdee80
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-07 10:51:16 +0900

    Update outputs.tf

--- Terraform/Part2/outputs.tf
+++ Terraform/Part2/outputs.tf
@@ -1,1 +1,9 @@
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+  sensitive = true
+}
 
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+  sensitive = true
+}
