12d495acf662b89b8e54161a5b623a5974231ebe
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-07 20:36:48 +0900

    Create main.tf

---
+++ Terraform/Part2/baston/main.tf
@@ -0,0 +1,49 @@
+resource "azurerm_public_ip" "bastonip" {
+  name                = "baston-public-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+
+
+resource "azurerm_network_interface" "bastonnic" {
+  name                = "baston-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "example-ip-config"
+    subnet_id                     = var.azurerm_subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.bastonip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                  = "example-vm"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example.id]
+  size                  = "Standard_DS1_v2"
+
+  admin_username = "exampleadmin"
+  admin_password = "ExampleP@ssword123!!"
+
+  disable_password_authentication = false
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  tags = {
+    environment = "Example"
+  }
+}
