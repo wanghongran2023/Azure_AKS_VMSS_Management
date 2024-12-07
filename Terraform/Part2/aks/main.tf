05f1e2a679ff3c7bb0bd1936d2a5040065e22c35
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-07 09:30:40 +0900

    Create main.tf

---
+++ Terraform/Part2/aks/main.tf
@@ -0,0 +1,42 @@
+
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_config.name
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = azurerm_resource_group.resource_group.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+  sensitive = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_config.name
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  dns_prefix          = "exampleaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+}
+
+resource "azurerm_role_assignment" "aks_acr_permission" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
